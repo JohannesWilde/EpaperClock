95ce9747518b4daeb3cba89a1d8fff6f5fb74413
Johannes Wilde <git@jowil.de>
2020-12-30 21:24:02 +0100

    Add some comment about consequences.

--- development/Interrupts/Interrupts.ino
+++ development/Interrupts/Interrupts.ino
@@ -31,13 +31,14 @@ void setup ()
   digitalWrite (BUTTON, HIGH);  // internal pull-up resistor
   attachInterrupt (digitalPinToInterrupt (BUTTON), switchPressed, CHANGE);  // attach interrupt handler
 
+  // this reconfigures TIMER2 - thus PWM [analogWrite] will not work as expected on Pins 3 and 11
   TCCR2A = 0b00000010; // COM2A1, COM2A0, COM2B1, COM2B0, 0, 0, WGM21, WGM20: no output, CTC-mode
   TCCR2B = 0b00000111; // FOC2A, FOC2B, 0, 0, WGM22, CS22, CS21, CS20: 1024 prescaler
   OCR2A = 49; // Output compare register [49 * 1024 @ 1MHz = 50176us]
   TIMSK2 = 0b00000010; // 0, 0, 0, 0, 0, OCIE2B, OCIE2A, TOIE2: Output Compare Match Interrupt Enable Timer2 A
   
   set_sleep_mode (SLEEP_MODE_PWR_SAVE);
-
+  
   sei();
 }  // end of setup
 
