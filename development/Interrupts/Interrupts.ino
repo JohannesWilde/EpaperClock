5f22c6ff30f0066009c98571c5148af365f87c7a
Johannes Wilde <git@jowil.de>
2020-12-30 22:13:30 +0100

    Be somewhat more careful as to volativle variables.

--- development/Interrupts/Interrupts.ino
+++ development/Interrupts/Interrupts.ino
@@ -47,9 +47,14 @@ void loop ()
 {    
   powerDown();
 
-  if (externalInterrupt)
+  noInterrupts();
+  bool const externallyInterrupted = externalInterrupt;
+  externalInterrupt = false;
+  interrupts();
+
+  
+  if (externallyInterrupted)
   {
-    externalInterrupt = false;
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(500);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
