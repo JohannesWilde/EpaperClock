19f96b2c9e36b2fc39f670b67a94824d92bbf9e0
Johannes Wilde <git@jowil.de>
2020-12-31 18:01:00 +0100

    Add files for incorporation into QtCreator - this is however only used for highlight definitions and cross-referencing. The actual compilation is done by the Arduino IDE.

--- EpaperClock/EpaperClock.ino
+++ EpaperClock/EpaperClock.ino
@@ -1,7 +1,4 @@
-// this is only for the QtCreator to find the correct highlighting - this is already defined somewhere before
-#ifndef __AVR_ATmega328P__
-#define __AVR_ATmega328P__
-#endif
+/* -*- C++ -*- Mark this as a c++ file for QtCreator [Preferences->Environment->MIME Types->text/x-c++src]. */
 
 #include "ArduinoDrivers/arduinoUno.hpp"
 
@@ -22,7 +19,7 @@ void setup()
 void loop() 
 {
 	// this shall not be reached - if however, then reset here.
-    void(* resetFunc) (void) = 0;//declare reset function at address 0
+    void(* resetFunc) (void) = 0; // declare reset function at address 0
 	resetFunc(); //call reset  
 }
 
---
+++ EpaperClock/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Very simple main-file provided solely for convenient highlighting in QtCreator.
+ * Please use the Arduino IDE for compiling EpaperClock.ino [this file will not be used].
+ * */
+
+
+#include <Arduino.h>
+
+// forward declarations
+void setup();
+void loop();
+
+int main()
+{
+    setup();
+	
+    while (true)
+    {
+        loop();
+    }
+}
