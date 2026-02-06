da3e047f4c7f1a13490ddc31f2adff37c3b226dc
Johannes Wilde <git@jowil.de>
2026-02-06 22:16:11 +0100

    Add comment.

--- helper.cpp
+++ helper.cpp
@@ -135,6 +135,7 @@ static std::vector<std::shared_ptr<Renderer2d>> renderers{
     // std::make_shared<Renderer2dAxesAlignedRectangle>(Coordinates2d::Position(8, 2), Coordinates2d::Position(19, 2), /*color*/ 0),
     // std::make_shared<Renderer2dAxesAlignedRectangle>(Coordinates2d::Position(6, 4), Coordinates2d::Position(6, 9), /*color*/ 0),
 
+    // Background - not necessary but only as to highlight spareSpaceOutside.
     std::make_shared<Renderer2dAxesAlignedRectangle>(Coordinates2d::Position(spareSpaceOutside, spareSpaceOutside),
                                                      Coordinates2d::Dimension(displayWidth - 2 * spareSpaceOutside, displayHeight - 2 * spareSpaceOutside), /*color*/ 255),
 };
